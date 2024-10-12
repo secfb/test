1e470a05a9d44237458f70a8e852a9d9f8b5b45c
Never Mind <38748801+secfb@users.noreply.github.com>
2024-10-12 22:52:31 +0300

    Update note.sh

--- note.sh
+++ note.sh
@@ -1,6 +1,6 @@
 Get-Content .\hostnames.txt | ForEach-Object { Resolve-DnsName $_ -Type A | Select-Object -ExpandProperty IPAddress }
 
-
+sudo timedatectl set-ntp off;sudo timedatectl set-timezone Asia/Istanbul ;date;timedatectl
 
 echo "ssh id_rsa.pub" >> ~/.ssh/authorized_keys
 
