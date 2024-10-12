3551fcd9383fd68df15e48af573137471c2a7247
Never Mind <38748801+secfb@users.noreply.github.com>
2024-10-12 22:35:06 +0300

    Update note.sh

--- note.sh
+++ note.sh
@@ -4,7 +4,4 @@ Get-Content .\hostnames.txt | ForEach-Object { Resolve-DnsName $_ -Type A | Sele
 
 echo "ssh id_rsa.pub" >> ~/.ssh/authorized_keys
 
-
-
-curl -A Mozilla -o all-2.0.tar.gz \
-  --url 'https://plugins.nessus.org/v2/nessus.php?f=all-2.0.tar.gz&u=4e2abfd83a40e2012ebf6537ade2f207&p=29a34e24fc12d3f5fdfbb1ae948972c6' &>/dev/null
+ssh-keygen -t rsa -b 4096
