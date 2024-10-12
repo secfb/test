e87243be46af36ee86e968542b12a082f5a95370
Never Mind <38748801+secfb@users.noreply.github.com>
2024-10-12 22:29:56 +0300

    Update note.sh

--- note.sh
+++ note.sh
@@ -6,4 +6,5 @@ echo "ssh id_rsa.pub" >> ~/.ssh/authorized_keys
 
 
 
-https://plugins.nessus.org/v2/nessus.php?f=all-2.0.tar.gz&u=4e2abfd83a40e2012ebf6537ade2f207&p=29a34e24fc12d3f5fdfbb1ae948972c6'
+curl -A Mozilla -o all-2.0.tar.gz \
+  --url 'https://plugins.nessus.org/v2/nessus.php?f=all-2.0.tar.gz&u=4e2abfd83a40e2012ebf6537ade2f207&p=29a34e24fc12d3f5fdfbb1ae948972c6' &>/dev/null
