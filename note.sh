247e2377b74d26ad6f2f2cad6cbf696fc4ec52b2
Never Mind <38748801+secfb@users.noreply.github.com>
2024-10-12 22:29:12 +0300

    Update note.sh

--- note.sh
+++ note.sh
@@ -3,3 +3,7 @@ Get-Content .\hostnames.txt | ForEach-Object { Resolve-DnsName $_ -Type A | Sele
 
 
 echo "ssh id_rsa.pub" >> ~/.ssh/authorized_keys
+
+
+
+https://plugins.nessus.org/v2/nessus.php?f=all-2.0.tar.gz&u=4e2abfd83a40e2012ebf6537ade2f207&p=29a34e24fc12d3f5fdfbb1ae948972c6'
